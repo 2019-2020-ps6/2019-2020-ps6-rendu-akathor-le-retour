5771b4ee4d6cf78ef7e75f6d134f4dcb319ecbff
safwane <safwane.benidir@etu.univ-cotedazur.fr>
2020-03-31 12:09:59 +0200

    #7 Prise en compte des réglages uniquement sur la page des réglages

--- frontend/src/app/settings/settings.component.ts
+++ frontend/src/app/settings/settings.component.ts
@@ -6,8 +6,26 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./settings.component.scss']
   })
   export class SettingsComponent implements OnInit {
+  mycolor: string;
+  mybackgroundcolor: string;
+  taille: string;
 
-    constructor() { }
+  styleSmall: string;
+  styleMedium: string;
+  styleBig: string;
+  style: string;
+
+
+    constructor() {
+      this.mycolor = 'black';
+      this.mybackgroundcolor = 'white';
+      this.taille = 'Taille';
+
+      this.styleSmall = '20px';
+      this.styleMedium = '25px';
+      this.styleBig = '30px';
+      this.style = '25px';
+    }
 
     ngOnInit() {
     }
@@ -16,4 +34,20 @@ import { Component, OnInit } from '@angular/core';
     console.log('goToAdminTest');
     }
 
+    switchStyle(style) {
+      document.getElementById('mycss').setAttribute('href', style);
+    }
+
+  changeColor() {
+    return {color: 'yellow', 'background-color': 'orange'};
   }
+
+  change(val1, val2) {
+    this.mycolor = val1;
+    this.mybackgroundcolor = val2;
+  }
+
+  changeSize(style: string) {
+    this.style = style;
+  }
+}
